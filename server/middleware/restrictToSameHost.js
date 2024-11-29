e9c4651d628e3b7eb60bb0192fe23bc21e00607c
Ernesto Diaz <zaidotsenre@gmail.com>
2024-11-28 22:28:55 -0500

    same host fixes

--- server/middleware/restrictToSameHost.js
+++ server/middleware/restrictToSameHost.js
@@ -1,16 +1,21 @@
-// middleware/restrictToSameHost.js
 const restrictToSameHost = (req, res, next) => {
-    const allowedPath = `/collections/${req.params.collectionId}/images`; // Open endpoint
+    const allowedPath = `/collections/${req.params?.collectionId}/images`; // Open endpoint
     const origin = req.get('Origin') || req.get('Referer') || '';
-
+    const serverHost = `${req.protocol}://${req.get('Host')}`;
+    
     // Allow open access for the specific endpoint
     if (req.path === allowedPath) {
         return next();
     }
 
-    // Check if the Origin or Referer matches the server's base URL
-    const serverHost = `${req.protocol}://${req.get('Host')}`;
-    if (origin.startsWith(serverHost)) {
+    // Allow requests proxied through Apache
+    const allowedHosts = [
+        'http://servifoto.ernestodiaz.dev',
+        'https://servifoto.ernestodiaz.dev',
+        serverHost, // Backend server
+    ];
+
+    if (allowedHosts.some(allowedHost => origin.startsWith(allowedHost))) {
         return next();
     }
 
